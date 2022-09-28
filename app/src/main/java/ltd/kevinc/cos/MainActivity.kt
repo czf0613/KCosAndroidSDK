55c30fadf55e5ff61073ee403d473cf0b8a67d9b
Kevin Chen <1354016594@qq.com>
2022-09-28 17:43:30 +0800

    init

---
+++ app/src/main/java/ltd/kevinc/cos/MainActivity.kt
@@ -0,0 +1,12 @@
+package ltd.kevinc.cos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ltd.kevinc.kcos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
