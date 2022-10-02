7dfb85860be2cf99d62d18af6562d5afc50e23de
Kevin Chen <1354016594@qq.com>
2022-10-03 04:10:57 +0800

    完善媒体编码器

--- KCosSDK/src/main/java/ltd/kevinc/kcos/KCosUtils.kt
+++ KCosSDK/src/main/java/ltd/kevinc/kcos/KCosUtils.kt
@@ -2,6 +2,7 @@ package ltd.kevinc.kcos
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import android.net.Uri
 import android.provider.MediaStore
@@ -14,7 +15,6 @@ import java.io.ByteArrayOutputStream
 /**
  * 媒体压缩工具，可以将各种媒体素材进行压缩然后进行上传
  * 这个类线程不安全，哪里用，哪里直接new，也是一个轻对象
- * 类里面写的方法虽然不一定有suspend修饰，但是调用时一定要想办法切线程
  * 1，图片会被转换成jpeg
  * 2，视频会被转换成h264，帧率被限30
  * 3，音频会被转换为aac
@@ -33,17 +33,6 @@ class KCosUtils {
     }
 
     /**
-     * @param filePath 输入文件的路径
-     * @return 返回图片的jpegData
-     */
-    private external fun convertImageWithOptions(
-        filePath: String,
-        width: Int,
-        height: Int,
-        jpegQuality: Int
-    ): ByteArray
-
-    /**
      * 特别小心，视频一旦被这里处理，会限制帧率为30
      * @param filePath 输入文件的路径
      * @return 输出文件的路径
@@ -59,10 +48,12 @@ class KCosUtils {
      * @param width 图片宽度，设置为非正数的时候，表示维持原图的宽度
      * @param height 图片高度，设置为非正数的时候，表示维持原图的高度
      * @return 返回jpegData，可以直接进行网络发送，或者重新使用Image进行decode
+     * @throws Exception 利用Bitmap的原生转码能力，可能由于安卓平台的实现问题，可能会出现各种各样的问题
      */
-    fun compressBitmap(bitmap: Bitmap, width: Int = -1, height: Int = -1): ByteArray {
+    suspend fun compressBitmap(bitmap: Bitmap, width: Int = -1, height: Int = -1): ByteArray {
         val expectedWidth = if (width <= 0) bitmap.width else width
         val expectedHeight = if (height <= 0) bitmap.height else height
+
         val scaleMatrix = Matrix().apply {
             postScale(
                 expectedWidth.toFloat() / bitmap.width,
@@ -70,16 +61,18 @@ class KCosUtils {
             )
         }
 
-        val tempBitmap =
-            Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, scaleMatrix, true)
-        bitmap.recycle()
+        return withContext(Dispatchers.Default) {
+            val tempBitmap =
+                Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, scaleMatrix, true)
 
-        return ByteArrayOutputStream().use { stream ->
-            tempBitmap.compress(Bitmap.CompressFormat.JPEG, jpegQuality, stream)
-            stream.flush()
-            tempBitmap.recycle()
+            ByteArrayOutputStream().use { stream ->
+                tempBitmap.compress(Bitmap.CompressFormat.JPEG, jpegQuality, stream)
+                stream.flush()
+                tempBitmap.recycle()
+                bitmap.recycle()
 
-            stream.toByteArray()
+                stream.toByteArray()
+            }
         }
     }
 
@@ -88,25 +81,27 @@ class KCosUtils {
      * 受支持的图片类型为png, jpg, webp和HEIC（不稳定）
      * heic的解码不一定能够成功，受制于硬件编码器的限制
      * @see compressBitmap 参数定义看此处定义
-     * @throws Exception 当文件的Uri无法被查询到的时候，会扔出一个异常，通常情况下不会发生这样的事情
+     * @throws java.io.FileNotFoundException 当文件的Uri无法被查询到的时候，会扔出一个异常，通常情况下不会发生这样的事情
+     * @throws Exception 解码或转码过程出现错误（一般是本地的native库出错，SDK暂无办法解决）
      */
-    fun compressPicture(uri: Uri, context: Context, width: Int = -1, height: Int = -1): ByteArray {
-        val filePath = context.contentResolver
-            .query(uri, arrayOf(MediaStore.Images.Media._ID), null, null, null, null)?.let {
-                it.use { cursor ->
-                    cursor.moveToFirst()
-                    val index = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                    cursor.getString(index)
-                }
-            } ?: uri.path
-
-        TODO("转码工具未完成")
+    suspend fun compressPicture(
+        uri: Uri,
+        context: Context,
+        width: Int = -1,
+        height: Int = -1
+    ): ByteArray {
+        return withContext(Dispatchers.IO) {
+            val bitmap = BitmapFactory.decodeStream(context.contentResolver.openInputStream(uri))
+            compressBitmap(bitmap, width, height)
+        }
     }
 
     /**
      * 将手机内的视频压缩成h264编码
      * @see compressPicture 参数类型基本一致
+     * @throws Exception 解码或转码过程出现错误（一般是本地的native库出错，SDK暂无办法解决）
      */
+    @Deprecated(message = "未开发完成，不要使用", level = DeprecationLevel.ERROR)
     suspend fun compressVideo(
         uri: Uri,
         context: Context,
@@ -115,16 +110,19 @@ class KCosUtils {
         delegate: KCosProcessDelegate
     ) {
         this.delegate = delegate
-//        val filePath = context.contentResolver
-//            .query(uri, null, null, null, null, null)?.let {
-//                it.use { cursor ->
-//                    cursor.moveToFirst()
-//                    val index = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-//                    cursor.getString(index)
-//                }
-//            } ?: uri.path
 
-        // 在计算核心上面跑性能会更好
+        val filePath = withContext(Dispatchers.IO) {
+            context.contentResolver
+                .query(uri, arrayOf(MediaStore.Images.Media._ID), null, null, null, null)?.let {
+                    it.use { cursor ->
+                        cursor.moveToFirst()
+                        val index = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                        cursor.getString(index)
+                    }
+                } ?: uri.path
+        }
+
+        // 在计算核心上面跑转码运算性能会更好
         withContext(Dispatchers.Default) {
             compressVideoJob?.cancel()
             compressVideoJob = launch {
@@ -134,12 +132,20 @@ class KCosUtils {
     }
 
     /**
-     * 将PCM音频转化为aac格式编码的音频
+     * 将MP3、FLAC等音频转化为aac格式编码的音频
+     * 原理是源文件 -> PCM -> aac音频
      * 请不要放进来非常长的声音文件！爆内存！
      * @param input PCM格式的原始音频素材
      * @param bitRate 输出的比特率（自带单位K），要求高一点就256，如果仅仅是发送语音消息的，设置为64就够了
      */
-    fun compressVoiceClip(input: ByteArray, bitRate: Int = 256): ByteArray {
-        TODO("转码工具未完成")
+    suspend fun compressAudioData(input: ByteArray, bitRate: Int = 256): ByteArray {
+        TODO()
+    }
+
+    /**
+     * 编码PCM数据到aac格式
+     */
+    suspend fun encodePCMAudioData(pcm: ByteArray, bitRate: Int = 256): ByteArray {
+        TODO()
     }
 }
--- app/src/main/java/ltd/kevinc/cos/MainActivity.kt
+++ app/src/main/java/ltd/kevinc/cos/MainActivity.kt
@@ -1,38 +1,37 @@
 package ltd.kevinc.cos
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
-import ltd.kevinc.kcos.KCosClient
-import ltd.kevinc.kcos.KCosFileDownloader
+import ltd.kevinc.kcos.KCosUtils
 import ltd.kevinc.kcos.R
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lifecycleScope.launch {
-            val userId = KCosClient.initializeKCosService(
-                "20b332a9-35a9-0b09-0bf2-79421bde4a54",
-                "e8c688ac-7a6e-1add-1a70-564a02fce624",
-                "1919810"
-            )
+//        lifecycleScope.launch {
+//            val userId = KCosClient.initializeKCosService(
+//                "20b332a9-35a9-0b09-0bf2-79421bde4a54",
+//                "e8c688ac-7a6e-1add-1a70-564a02fce624",
+//                "1919810"
+//            )
+//
+//            println(userId)
 
-            println(userId)
-
-            val downloader = KCosFileDownloader()
-            downloader.downloadViaSystemDownloadManager(44L, this@MainActivity)
-        }
+//            val downloader = KCosFileDownloader()
+//            val fileId = downloader.downloadViaSystemDownloadManager(44L, this@MainActivity)
+//        }
     }
 
     override fun onStart() {
         super.onStart()
 
         lifecycleScope.launch {
-            val file = File("${applicationContext.dataDir.path}/fake.dat")
+//            val file = File("${applicationContext.dataDir.path}/fake.dat")
 //            file.createNewFile()
 //
 //            for (i in 0..10240) {
@@ -40,7 +39,15 @@ class MainActivity : AppCompatActivity() {
 //
 //                println("writing $i KB")
 //            }
-            println(file.length())
+//            println(file.length())
+
+            val stream = assets.open("test_pic.heif")
+            val bitmap = BitmapFactory.decodeStream(stream)
+            println(bitmap.byteCount)
+
+            val decoder = KCosUtils()
+            val jpegData = decoder.compressBitmap(bitmap)
+            println(jpegData.size)
 
 //            val uploader = KCosFileUploader()
 //
